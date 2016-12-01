86d62a5d8d37a3e03e2401c4c3367eed3abe946b
Ivan Rgz <jorgega@herbalife.com>
2016-12-01 11:58:44 -0600

    comments or work done

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
+/**************************************************
+
+Hi There Arturo
+as you can see the back is not complete , this core is more regarding with
+make the request to the Database and Handle the Data.
+I used for testing this REST REST Easy for Google Chrome.
+
+Mongodb
+DBS : hobbies !made it for test porpuse
+Collection : hobbies !made it for test porpuse
+
+Rest API:
+Content-Type : application/json
+* http://localhost:3000/api/hobbies -> Method GET -> Getting the Info
+* http://localhost:3000/api/hobbies -> Method Post -> Create a Record
+      {
+      "hobbie":"Test Hobby Update",
+      "hobbie_type": "Test Hobbie Type Update"
+      }
+* http://localhost:3000/api/_id_from_database -> Method Put -> Update a Record
+      {
+      "hobbie":"Test Hobby Update",
+      "hobbie_type": "Test Hobbie Type Update"
+      }
+* http://localhost:3000/api/_id_from_database -> Method Delete -> Delete a Record
+
+Left to Do :
+
+* All the front end
+* Add the Middleware and the Cookie , i thought would be better add once have the front
+in progress.
+* For Testing porpuse i like it to use Grunt and Mocha , also thought would better
+used it once the front is in progress
+
+Any Comment, Suggest or Concern
+
+Let me know by Skype or with a commit
+
+Thanks
+Sincerily
+Ivan
+
+*****************************************************/
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
